b6088dc227d92e5c4a5ccfb80e64c1513ee827bd
Max Knudsen <maxknudsen501@gmail.com>
2024-11-16 11:41:16 -0500

    Added script for managing destinations and started prototyping system for correct destinations

--- WolfJam24/Assets/Scripts/Destination.cs
+++ WolfJam24/Assets/Scripts/Destination.cs
@@ -6,25 +6,51 @@ public class Destination : MonoBehaviour
 {
 
     private string destName;
+    private bool isCorrect = false;
+    private bool inside = false;
+    public string Name => destName;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         destName = gameObject.name;
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void SetCorrect(bool correct) {  isCorrect = correct; } 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            // Delivery stuff
-            print("Now arriving at " + destName);
-        }
+        // Delivery stuff
+        print("Now arriving at " + destName);   
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        inside = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inside = false;
+    }
+
+    public void DeliverPackage()
+    {
+        if (isCorrect) print("You did it!");
+        else print("You failed");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (inside && Input.GetKeyDown(KeyCode.E))
+        {
+            DeliverPackage();
+        }
     }
 }
