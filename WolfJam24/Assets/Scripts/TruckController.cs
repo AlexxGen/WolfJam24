3b0eceec36e09a1c96c9cb725f0d95b36157a9ad
Max Knudsen <maxknudsen501@gmail.com>
2024-11-16 10:39:29 -0500

    Added driving scene and script
    Truck go vroom

---
+++ WolfJam24/Assets/Scripts/TruckController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour
+{
+
+    // Constants
+    public float speed;
+    public float turnSpeed;
+
+    // Components
+    private Rigidbody2D rb;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int movement = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement--;
+        }
+        if (movement != 0)
+        {
+            int angular = 0;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                angular--;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                angular++;
+            }
+
+            rb.velocity = movement * speed * transform.up;
+            rb.angularVelocity = angular * turnSpeed;
+        }
+        else rb.angularVelocity = 0;
+    }
+}
