15d008b2d5f440c9f54adc50ecbdf9df31dc77f2
Max Knudsen <maxknudsen501@gmail.com>
2024-11-16 21:16:04 -0500

    Added BG control to cutscene script

--- WolfJam24/Assets/Scripts/CutsceneHandler.cs
+++ WolfJam24/Assets/Scripts/CutsceneHandler.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CutsceneHandler : MonoBehaviour
 {
-
+    public Image[] goodImages;
+    public Image[] badImages; 
     public float cutsceneLength;
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < goodImages.Length; i++)
+        {
+            goodImages[i].enabled = false;
+            badImages[i].enabled = false;
+        }
+
+        if (GameManager.Instance.deliveredGood) goodImages[GameManager.Instance.CurrentPackage].enabled = true;
+        else badImages[GameManager.Instance.CurrentPackage].enabled = true;
         StartCoroutine(WaitThenChangeScene());
     }
 
